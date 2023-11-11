dd4bff699ac71378543e0115b24f65ecfba0d147
Praniyal Thapa <praniyalthapa100@gmail.com>
2023-11-11 13:10:01 +0545

    added jwt token services and tested and verified it

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ const {PORT}=require('./config/serverConfig');
 const bodyParser=require('body-parser');
 const app=express();
 const apiRoutes=require('./routes/index');
-// const {User}=require('./models/index');
-// const bcrypt=require('bcrypt');
-//const UserRepository=require('./repository/user-repository');
+const userService=require('./services/user-service');
 const user = require('./models/user');
+//const UserService = require('./services/user-service');
 const prepareAndStartServer=()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +19,13 @@ const prepareAndStartServer=()=>{
         // const incomingPwd='123453';
         // const hash=await User.findByPk(1);
         // const response=bcrypt.compareSync(incomingPwd,hash);
+        // const service=new UserService();
+        //  const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiBwcmFuaXlhbDEwMEBnbWFpbC5jb20iLCJpZCI6MiwiaWF0IjoxNjk5Njg2OTE3LCJleHAiOjE2OTk2ODY5NDd9.UUtXmJFJs2uNwOaCPaoi_eMyGb5lQSX6vkcOt16-RqY';
+        //  const response=service.verifyToken(token);
+        //  console.log(response);
+        // const newToken=service.createToken({email:' praniyal100@gmail.com',id:2});
+        // console.log("New token created now is :",newToken);
+
     });
 
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository=require('../repository/user-repository');
+const {JWT_KEY}=require('../config/serverConfig');
+const jwt = require('jsonwebtoken');
 class UserService{
     constructor(){
         this.userRepository=new UserRepository();
@@ -13,5 +15,31 @@ class UserService{
     
    }
     }
+     createToken(user){ //using this user object we are going to create token 
+      try {
+        const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' }); //jwt.sign takes two argument one is user and next one is KEY which is in my .env file and next argument tell timing to expire jwt token
+            return result;
+        
+      } catch (error) {
+        
+        console.log("Error occured!");
+        throw error;
+      }
+
+
+    }
+verifyToken(token){
+    try {
+        const response=jwt.verify(token,JWT_KEY); //in argument we pass token and same jwt_key from which we created token 
+        return response;
+        
+    } catch (error) {
+        console.log("Oops! Something went wrong!",error);
+        throw error;
+        
+    }
+
+}
+
 }
 module.exports=UserService;
